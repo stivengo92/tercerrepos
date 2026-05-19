2d21c360507dd2ba954313bb4aea10ef9d736f76
stiven <stivn.4@gmail.com>
2026-05-19 18:12:42 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerrepos",
+    version='0.1.0',
+    packages=find_packages(),
+    description="un paquete pip seimple de saludo",
+    author="stiven gomez",
+    author_email="stivn.4@gmail.com",
+    url="https://github.com/stivengo92/tercerrepos",
+
+)
